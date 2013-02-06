349ef2ed2a47e2fcc5796984a8c12edc62acd798
Jens Thomas <jetho@gmx.de>
2013-02-06 21:48:10 +0100

    Added Evaluator

--- src/main/scala/org/jetho/scms/Environment.scala
+++ src/main/scala/org/jetho/scms/Environment.scala
@@ -19,7 +19,7 @@ class Environment(ids: Map[String, Exp], parent: Option[Environment]) {
   /** Search up the scope stack for a given identifier.
       Apply the action function to the scope which contains the identifier.
       Fail if the scope stack doesn't contain a valid binding for the identifier.*/
-  private def forScopeOf(id: String)(action: (Scope => Exp)): \/[String, Exp] =
+  private def forScopeOf(id: String)(action: (Scope => Exp)): String \/ Exp =
     if (ids contains id)
       action(ids).right
     else
