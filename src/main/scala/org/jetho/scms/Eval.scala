51f7ea96c5f5d194b30289714bac78778bde0243
unknown <thomas@Thomas-W7.dodav.int.odav.de>
2013-02-06 16:34:46 +0100

    Added dotted lists

---
+++ src/main/scala/org/jetho/scms/Eval.scala
@@ -0,0 +1,52 @@
+
+ /** The Evaluator.*/
+
+
+package org.jetho.scms
+
+
+import scalaz._
+import Scalaz._
+
+
+object Eval {
+
+  type Primitive = List[Exp] => Exp
+
+ 
+  def eval(exp: Exp): \/[String, Exp] = exp match {
+    case n @ NumExp(_) => n.right
+    case s @ StringExp(_) => s.right
+    case b @ BoolExp(_) => b.right
+    case ListExp(List(SymbolExp("quote"), rest)) => rest.right
+    case ListExp(SymbolExp(f) :: args) => args.map(eval).sequenceU >>= applyFunc(f)
+    case _ => s"Unknown Expression $exp".left
+  }
+
+  def applyFunc(func: String)(args: List[Exp]) =
+    primitives(func)(args).right
+
+  def numericBinOp(op : (Int, Int) => Int)(args: List[Exp]) = {
+    val (first :: rest) = args map unpackNum
+    NumExp(rest.foldLeft(first)(op))
+  }
+    
+  def unpackNum(e: Exp): Int = e match {
+    case NumExp(n) => n
+    case StringExp(s) => s.toInt
+    case ListExp(List(n)) => unpackNum(n)
+    case _ => 0
+  }
+
+  val primitives: Map[String, Primitive] = 
+    Map( 
+      "+" -> numericBinOp (_ + _),
+      "-" -> numericBinOp (_ - _),
+      "*" -> numericBinOp (_ * _),
+      "/" -> numericBinOp (_ / _),
+      "mod" -> numericBinOp (_ % _),
+      "quotient" -> numericBinOp(_ / _),
+      "remainder" -> numericBinOp(_ % _) 
+    )  
+}
+  
--- src/main/scala/org/jetho/scms/Reader.scala
+++ src/main/scala/org/jetho/scms/Reader.scala
@@ -10,37 +10,30 @@ import scalaz._
 import Scalaz._
 
 
-
 object Reader extends RegexParsers {
 
-  private def space  = "[ \\n]*".r
-
-  private def symbol: Parser[Exp] = """[a-zA-Z!#$%&|*+/:<=>?@^_~-]\w*""".r ^^ { 
+  private def atom = """[a-zA-Z!#$%&|*+-/:<=>?@^_~][a-zA-Z0-9!#$%&|*+-/:<=>?@^_~]*""".r ^^ { 
     case "#t" => BoolExp(true)
     case "#f" => BoolExp(false)
     case sym => SymbolExp(sym)
   }
 
-  private def string: Parser[StringExp] = "\"" ~> rep("[^\"]".r) <~ "\"" ^^ { s => StringExp(s.mkString) }
+  private def num = """-?\d+""".r ^^ { n => NumExp(n.toInt) }
 
-  private def number: Parser[NumExp] = "[0-9]+".r ^^ { n => NumExp(n.toInt) }
+  private def str = "\"" ~> rep("[^\"]".r) <~ "\"" ^^ { s => StringExp(s.mkString) }
 
-  private def list: Parser[ListExp] = rep(expr) ^^ { ListExp }
+  private def quoted = "'" ~> expr ^^ { e => ListExp(List(SymbolExp("quote"), e)) }
 
-  private def dottedList : Parser[DottedListExp] = 
-    rep(expr) ~ "." ~ expr ^^ { case head ~ "." ~ tail => new DottedListExp(head, tail) }
+  private def list = rep(expr) ^^ { ListExp }
 
-  private def quoted: Parser[ListExp] = "'" ~> expr ^^ { e => ListExp(List(SymbolExp("quote"), e)) }
+  private def dottedList = rep(expr) ~ "." ~ expr ^^ { case head ~ "." ~ tail => new DottedListExp(head, tail) }
 
-  private def expr: Parser[Exp] = 
-    symbol | string | number | quoted | '(' ~> (dottedList | list) <~ space <~ ')'
+  private def expr : Parser[Exp] = atom | str | num | quoted | "(" ~> (list | dottedList) <~ ")"
 
-
+    
   def apply(input: String): \/[String, Exp] =
     parse(expr, input) match {
       case Success(res, _) => res.right
       case NoSuccess(msg, _) => msg.left 
     }
-  
 }
-  
