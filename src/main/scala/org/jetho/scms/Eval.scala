4b42c30d3dd18d3252d2f6e2ff68b19bb89b4327
unknown <thomas@Thomas-W7.dodav.int.odav.de>
2013-02-07 14:53:04 +0100

    Added error checking

--- src/main/scala/org/jetho/scms/Eval.scala
+++ src/main/scala/org/jetho/scms/Eval.scala
@@ -11,35 +11,36 @@ import Scalaz._
 
 object Eval {
 
-  type Primitive = List[Exp] => Exp
+  type Result[A] = String \/ A  
+  type Primitive[A] = List[Exp] => Result[A]
 
-  def eval(exp: Exp): String \/ Exp = exp match {
+
+  def eval(exp: Exp): Result[Exp] = exp match {
     case n @ NumExp(_) => n.right
     case s @ StringExp(_) => s.right
     case b @ BoolExp(_) => b.right
     case ListExp(List(SymbolExp("quote"), rest)) => rest.right
     case ListExp(SymbolExp(f) :: args) => args.map(eval).sequenceU >>= applyFunc(f)
-    case _ => s"Unknown Expression $exp".left
+    case _ => s"Unknown Expression: $exp".left
   }
 
   def applyFunc(id: String)(args: List[Exp]) =
+    toRight(primitives.get(id))(s"Unknown Function: $id") >>= (_ apply args)
+
+  def numericBinOp(op : (Int, Int) => Int)(args: List[Exp]) = 
     for {
-      f <- toRight(primitives.get(id))(s"Unknown Function $id")
-    } yield f(args)
-
-  def numericBinOp(op : (Int, Int) => Int)(args: List[Exp]) = {
-    val (first :: rest) = args map unpackNum
-    NumExp(rest.foldLeft(first)(op))
-  }
+      params <- args.map(unpackNum).sequenceU
+      val (first :: rest) = params
+    } yield NumExp(rest.foldLeft(first)(op))
     
-  def unpackNum(e: Exp): Int = e match {
-    case NumExp(n) => n
-    case StringExp(s) => s.toInt
+  def unpackNum(e: Exp): Result[Int] = e match {
+    case NumExp(n) => n.right
+    case StringExp(s) => try { s.toInt.right } catch { case _: Throwable => s"Illegal Number: $s".left }
     case ListExp(List(n)) => unpackNum(n)
-    case _ => 0
+    case _ => s"Illegal Number: $e".left
   }
 
-  val primitives: Map[String, Primitive] = 
+  val primitives: Map[String, Primitive[Exp]] = 
     Map( 
       "+" -> numericBinOp (_ + _),
       "-" -> numericBinOp (_ - _),
