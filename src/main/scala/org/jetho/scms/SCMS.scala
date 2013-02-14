7ebb13c5a02fa976259832189e12d032d1db220c
unknown <thomas@Thomas-W7.dodav.int.odav.de>
2013-02-14 13:36:22 +0100

    Added REPL

---
+++ src/main/scala/org/jetho/scms/SCMS.scala
@@ -0,0 +1,53 @@
+
+package org.jetho.scms
+
+import scalaz._
+import Scalaz._
+import java.io._
+import java.util.Scanner
+import scala.annotation.tailrec
+
+
+object SCMS {
+
+  val scanner = new Scanner(System.in)
+
+  @tailrec 
+  def until[A](pred: A => Boolean)(prompt: => A)(action: A => Unit): Unit = {
+    val res = prompt
+    if (pred(res)) return ()
+    else {
+      action(res)
+      until(pred)(prompt)(action)
+    }
+  }
+
+  def runRepl {
+    println(welcomeMsg)
+    until[String](_ == "quit")(readPrompt("SCMS>>> "))(evalAndPrint)
+    println(quitMsg)
+  }
+
+  def readPrompt(s: String): String = {
+    print(s); System.out.flush()
+    scanner.nextLine
+  }
+
+  def evalAndPrint(expr: String): Unit = {
+    val res = Reader.read(expr) >>= Eval.eval
+    res.fold(errMsg => println("Error: " + errMsg), println)
+  }
+  
+
+  def main(args: Array[String]) {
+    args.toList match {
+      case Nil => runRepl
+      case expression :: Nil => evalAndPrint(expression)
+      case _ => println("Program takes only 0 or 1 argument") 
+    }
+  }
+
+
+  val welcomeMsg = "\n - Welcome to SMCS! -\n"
+  val quitMsg = "Bye!\n"
+}
