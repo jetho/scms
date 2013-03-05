d725650f193bb43c53aa58af9225c1dc78b9b9d3
unknown <thomas@Thomas-W7.dodav.int.odav.de>
2013-03-05 14:46:57 +0100

    Added new Parser Tests

--- src/test/scala/org/jetho/scms/ReaderSpec.scala
+++ src/test/scala/org/jetho/scms/ReaderSpec.scala
@@ -7,44 +7,55 @@ import scalaz._
 import Scalaz._
 
 
-
 class ReaderSpec extends Specification {
 
   "The Reader" should {
-    "parse a number" in {
-      Reader.readExpr("23") must beEqualTo(NumExp(23).success)
+    "parse numbers" in {
+      Reader.readExpr("23") must beEqualTo(NumExp(23).right)
     }
-    "parse a string" in {
-      Reader.readExpr("\"Test\"") must beEqualTo(StringExp("Test").success)
+    "parse strings" in {
+      Reader.readExpr("\"Test\"") must beEqualTo(StringExp("Test").right)
     }
-    "parse a symbol" in {
-      Reader.readExpr("cons") must beEqualTo(SymbolExp("cons").success)
+    "parse symbols" in {
+      Reader.readExpr("cons") must beEqualTo(SymbolExp("cons").right)
     }
-    "parse a boolean true" in {
-      Reader.readExpr("#t") must beEqualTo(BoolExp(true).success)
+    "parse boolean true literals" in {
+      Reader.readExpr("#t") must beEqualTo(BoolExp(true).right)
     }
-    "parse a boolean false" in {
-      Reader.readExpr("#f") must beEqualTo(BoolExp(false).success)
+    "parse boolean false literals" in {
+      Reader.readExpr("#f") must beEqualTo(BoolExp(false).right)
     }
-    "parse a quoted list" in {
-      Reader.readExpr("'(1 2 3)") must beEqualTo(ListExp(List(SymbolExp("quote"), ListExp(List(NumExp(1), NumExp(2), NumExp(3))))).success)
+    "parse quoted expressions" in {
+      Reader.readExpr("'(1 2 3)") must beEqualTo(ListExp(List(SymbolExp("quote"), ListExp(List(NumExp(1), NumExp(2), NumExp(3))))).right)
     }
-    "parse a list" in {
-      Reader.readExpr("(+ 2 3)") must beEqualTo(ListExp(List(SymbolExp("+"), NumExp(2), NumExp(3))).success)
+    "parse lists" in {
+      Reader.readExpr("(+ 2 3)") must beEqualTo(ListExp(List(SymbolExp("+"), NumExp(2), NumExp(3))).right)
     }
-    "parse a nested list" in {
-      Reader.readExpr("(cons 1 (cons 2 3))") must beEqualTo(ListExp(List(SymbolExp("cons"), NumExp(1), ListExp(List(SymbolExp("cons"), NumExp(2), NumExp(3))))).success)
+    "parse dotted lists" in {
+      Reader.readExpr("(+ 1 . (2 3))") must beEqualTo(DottedListExp(List(SymbolExp("+"), NumExp(1)), ListExp(List(NumExp(2), NumExp(3)))).right)
+    }
+    "parse nested lists" in {
+      Reader.readExpr("(cons 1 (cons 2 3))") must beEqualTo(ListExp(List(SymbolExp("cons"), NumExp(1), ListExp(List(SymbolExp("cons"), NumExp(2), NumExp(3))))).right)
     } 
-    "parse a combined expression" in {
+    "parse combined expressions" in {
       Reader.readExpr("(if (= x 1) (+ x y) (+ x z))") must beEqualTo(ListExp(List(SymbolExp("if"),
                                                                          ListExp(List(SymbolExp("="), SymbolExp("x"), NumExp(1))),
                                                                          ListExp(List(SymbolExp("+"), SymbolExp("x"), SymbolExp("y"))),
-                                                                         ListExp(List(SymbolExp("+"), SymbolExp("x"), SymbolExp("z"))))).success)
+                                                                         ListExp(List(SymbolExp("+"), SymbolExp("x"), SymbolExp("z"))))).right)
     }              
+    "parse lists of expressions" in {
+      Reader.readExprList("(cons 1 (cons 2 3)) (+ 1 2) (define a 1)") must beEqualTo(
+        List(
+          ListExp(List(SymbolExp("cons"), NumExp(1), ListExp(List(SymbolExp("cons"), NumExp(2), NumExp(3))))),
+          ListExp(List(SymbolExp("+"), NumExp(1), NumExp(2))),
+          ListExp(List(SymbolExp("define"), SymbolExp("a"), NumExp(1)))
+        ).right)          
+    } 
     "fail for invalid expressions" in {
-      Reader.readExpr("(if (= x 1) (+ x y) (+ x y)") must beAnInstanceOf[Failure[String, Exp]]
+      Reader.readExpr("(if (= x 1) (+ x y) (+ x y)") must beAnInstanceOf[scalaz.-\/[_]]
+      Reader.readExprList("(+ 1 2) (if (= x 1) (+ x y) (+ x y) (define a 1)") must beAnInstanceOf[scalaz.-\/[_]]
+      Reader.readExprList("(if (= x 1) (+ x y) (+ x y) (define a") must beAnInstanceOf[scalaz.-\/[_]]
     }
     
   }
-  
 }
